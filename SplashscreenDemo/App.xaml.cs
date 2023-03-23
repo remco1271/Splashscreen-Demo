7f0676a713ded2a0a8f3ce3f1399d5ab529146b3
Remco Fischer <6353023+remco1271@users.noreply.github.com>
2023-03-23 16:21:47 +0100

    All code

---
+++ SplashscreenDemo/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SplashscreenDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public SplashScreen SplashScreen { get; private set; }
+        public App()
+        {
+            SplashScreen = new SplashScreen("resources/SplashScreen.png");
+            // run Squirrel first, as the app may exit after these run
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: OnAppInstall,
+                onAppUninstall: OnAppUninstall,
+                onEveryRun: OnAppRun);
+        }
+        private void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // Start showing Splashscreen and autoclose
+            SplashScreen.Show(true);
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
+        }
+    }
+}
---
+++ SplashscreenDemo/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Squirrel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SplashscreenDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string labelText = "Main Windows2";
+
+        public string LabelText
+        {
+            get => labelText; set
+            {
+                labelText = value;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            info.currentMainWindow = this;
+            Task.Run(UpdateMyApp);
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Update()
+        {
+            LabelText = "Update Available";
+        }
+        public void NoUpdate()
+        {
+            LabelText = "Up to date";
+        }
+        public void WaitUpdate()
+        {
+            LabelText = "Wait..";
+        }
+        private async Task UpdateMyApp()
+        {
+            info.currentMainWindow.WaitUpdate();
+            using var mgr = new UpdateManager(@"C:\Users\RemcoFischerHetronic\source\repos\SplashscreenDemo\SplashscreenDemo\bin\Release\net6.0-windows\Releases");
+            var newVersion = await mgr.UpdateApp();
+
+            // optionally restart the app automatically, or ask the user if/when they want to restart
+            if (newVersion != null)
+            {
+                info.currentMainWindow.Update();
+                //UpdateManager.RestartApp();
+            }
+            else
+                info.currentMainWindow.NoUpdate();
+        }
+    }
+}
